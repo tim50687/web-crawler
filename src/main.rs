45199b85b64567842eed6e3d244a592878e16468
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-03-13 19:53:15 -0400

    implemented tls stream and get root page html

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 
 mod util;
+mod tls_util;
 use util::parse_arguments;
 
+use tls_util::{connect_tls, send_message, read_message};
 fn main() {
     // Get the parsed arguments
     let _matches = parse_arguments();
@@ -11,8 +13,19 @@ fn main() {
     let username = _matches.get_one::<String>("username").map(String::as_str).unwrap();
     let password = _matches.get_one::<String>("password").map(String::as_str).unwrap();
 
-    println!("Server: {}", server);
-    println!("Port: {}", port);
-    println!("Username: {}", username);
-    println!("Password: {}", password);
+    // Get the TLS stream
+    let mut stream = connect_tls(server, port).expect("Failed to connect");
+
+    // Try to get the home page
+    let request = format!("GET / HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n", server);
+
+    send_message(&mut stream, &request); 
+
+    // Read the response
+    let response = read_message(&mut stream);
+
+    println!("{}", response);
+    
 }
+
+
---
+++ src/tls_util.rs
@@ -0,0 +1,25 @@
+use std::net::TcpStream;
+use std::io::{BufReader, Read, Write};
+use native_tls::{ TlsStream, TlsConnector};
+
+// Create TLS stream
+pub fn connect_tls(host: &str, port: &str) -> Result<TlsStream<TcpStream>, Box<dyn std::error::Error>> {
+    let stream = TcpStream::connect(format!("{}:{}", host, port))?;
+    let connector = TlsConnector::new()?;
+    let stream = connector.connect(host, stream)?;
+    Ok(stream)
+}
+
+// Send message
+pub fn send_message(stream: &mut TlsStream<TcpStream>, message: &str) -> () {
+    let req_bytes = message.as_bytes();
+    stream.write(req_bytes).unwrap();
+}
+
+// Read message
+pub fn read_message(stream: &mut TlsStream<TcpStream>) -> String {
+    let mut response = String::new();
+    let mut conn = BufReader::new(stream);
+    conn.read_to_string(&mut response).unwrap();
+    response
+}
