792a74539a6f774fd9fb2bf5333cedeabb2f7ee9
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-03-12 21:55:41 -0400

    implemented argument parsing

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use clap::{Arg, Command};
+
+mod util;
+use util::parse_arguments;
+
+fn main() {
+    // Get the parsed arguments
+    let _matches = parse_arguments();
+    // Get the values of the arguments
+    let server = _matches.get_one::<String>("server").map(String::as_str).unwrap();
+    let port = _matches.get_one::<String>("port").map(String::as_str).unwrap();
+    let username = _matches.get_one::<String>("username").map(String::as_str).unwrap();
+    let password = _matches.get_one::<String>("password").map(String::as_str).unwrap();
+
+}
---
+++ src/util.rs
@@ -0,0 +1,17 @@
+use clap::{Arg, Command};
+
+// This function will return the parsed arguments
+pub fn parse_arguments() -> clap::ArgMatches {
+    Command::new("web crawler")
+    .arg(Arg::new("server")
+        .short('s')
+        .default_value("www.3700.network"))
+    .arg(Arg::new("port")
+        .short('p')
+        .default_value("443"))
+    .arg(Arg::new("username")
+        .required(true))
+    .arg(Arg::new("password")
+        .required(true))
+    .get_matches()
+}
